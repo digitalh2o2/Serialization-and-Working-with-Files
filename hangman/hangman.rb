d14476bdfc17cb46afadd2b11b4e070d7dd7c7c4
Saul Ocampo <digitalh2o@gmail.com>
2016-02-09 22:44:23 -0600

    Random word generates now

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,6 +1,24 @@
-puts "Welcome to the game of Hangman!!"
+#hangman class that will hold the methods
+class Hangman
+	attr_accessor :name
 
+	def initialize(name)
+		@name = name
+	end
+end
 
-words = File.read("5desk.txt")
-	
-#puts words
+def dictionary
+	words = File.readlines("5desk.txt")
+	random = words.select {|w| w.size >= 4 && w.size <= 13}.sample
+end
+
+def start_game
+	puts "Welcome to the game of Hangman!!"
+	puts "What is your name?"
+	name = gets.chomp.capitalize
+	@name = Hangman.new(name)
+	puts "Hello #{@name.name}!"
+	puts dictionary	
+end
+
+start_game
